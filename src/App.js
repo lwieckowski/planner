41bdd6a69173cb46efa73ae99c48521fb96ecf5a
lwieckowski <lwieckowski@outlook.com>
2023-03-26 09:17:56 +0200

    Create app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Planner app</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
