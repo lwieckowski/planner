5e4144353040e2f4b34462df8059cba4dd176a37
lwieckowski <lwieckowski@outlook.com>
2023-04-30 19:46:32 +0200

    Add delete button on hover

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,13 +1,29 @@
-import { ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { ListItem, ListItemButton, ListItemIcon, ListItemText, IconButton } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 export default function MenuItem({ text, icon}) {
   return (
-    <ListItem disablePadding>
+    <ListItem disablePadding sx={
+      {
+        '& .delete-button': {
+          visibility: 'hidden'
+        },
+        '&:hover .delete-button': {
+          visibility: 'visible'
+        }
+      }
+      }>
       <ListItemButton disableRipple>
         <ListItemIcon>
           {icon}
         </ListItemIcon>
         <ListItemText primary={text} />
+        <ListItemIcon className="delete-button">
+          <IconButton aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        </ListItemIcon>
       </ListItemButton>
     </ListItem>
   );
